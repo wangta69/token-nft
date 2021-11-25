3771631b50e2f69fbe40579a701a666a1809f8f0
younghyeong ryu <wangta69@naver.com>
2021-11-25 10:41:05 +0900

    t
    t

--- ntf/uploadFile.js
+++ ntf/uploadFile.js
@@ -1,5 +1,5 @@
-const pinataApiKey = "a8e2fff1fe45d92846de";
-const pinataSecretApiKey = "c22feeae58c7c063473ba1e782826a45696283ae85001fbc68fc7ab0379e3d54";
+const pinataApiKey = "YOUR pinata api key";
+const pinataSecretApiKey = "Your pinata Secret Api Key";
 const axios = require("axios");
 const fs = require("fs");
 const FormData = require("form-data");
